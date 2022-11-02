48b73c5fa4a1ea7089bd6ea2cb2cf0bb7cdfedb6
hoffl1022 <113646106+hoffl1022@users.noreply.github.com>
2022-11-03 12:14:13 +1300

    Add files via upload

---
+++ 01_Looping_Basic.py
@@ -0,0 +1,12 @@
+
+valid = False
+while not valid:
+
+    response = float(input("enter a number: "))
+
+    if response > 0:
+        valid = True
+
+    else:
+        print("please enter a number that is more than zero")
+        print()
