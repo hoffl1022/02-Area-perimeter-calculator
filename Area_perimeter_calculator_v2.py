1863b12d1d2272cf1f29f93a9256716256003d57
hoffl1022 <113646106+hoffl1022@users.noreply.github.com>
2022-11-21 09:11:46 +1300

    Add files via upload

---
+++ Area_perimeter_calculator_v2.py
@@ -0,0 +1,47 @@
+# functions go here
+
+# checks input is a number more than zero
+def num_check(question):
+    valid = False
+    while not valid:
+
+        error = "please enter a number that is more than zero"
+
+        try:
+
+            # ask user to enter a number
+            response = float(input(question))
+
+            # checks number is more than zero
+            if response > 0: 
+                return response
+
+            # outputs error if input is invalid
+            else:
+                print(error)
+                print()
+
+        except ValueError:
+            print(error)
+
+
+
+# main routine goes here
+
+keep_going = ""
+while keep_going == "":
+
+    width = num_check("Width: ")
+    height = num_check("Height: ")
+
+    # calculate area (width x height)
+    area = width * height
+
+    # calculate perimeter (width + height) x 2
+    perimeter = 2 * (width + height)
+
+    # output area and perimeter
+    print("Area: {} square units".format(area))
+    print("Perimeter: {} units".format(perimeter))
+
+    keep_going = input("press <enter> to keep going or any key to quit")
